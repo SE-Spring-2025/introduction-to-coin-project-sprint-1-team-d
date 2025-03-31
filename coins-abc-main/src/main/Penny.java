5fa7ca4524e318c73f252153d00da5441993a53d
malloryr810 <malloryr810@gmail.com>
2025-03-31 00:21:24 -0400

    Deleted unnecessary files and added correct zip

--- coins-abc-main/src/main/Penny.java
+++
@@ -1,8 +0,0 @@
-public class Penny extends Coin {
-    public Penny(int year) {
-        super(0.01, "Penny", "IN GOD WE TRUST", year,
-              "A_Lincoln", "Lincoln_Memorial", "E PLURIBUS UNUM",
-              "LIBERTY", "UNITED STATES OF AMERICA", "ONE CENT",
-              false, "Copper");
-    }
-}
