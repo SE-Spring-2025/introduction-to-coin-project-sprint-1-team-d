40d2c9995b57513c93faacf34c3d97e84451b8ef
GreysonA75 <thegreysonj@gmail.com>
2025-03-30 23:29:53 -0400

    Refactored Coin.java and added coin subclasses

---
+++ coins-abc-main/src/main/Penny.java
@@ -0,0 +1,8 @@
+public class Penny extends Coin {
+    public Penny(int year) {
+        super(0.01, "Penny", "IN GOD WE TRUST", year,
+              "A_Lincoln", "Lincoln_Memorial", "E PLURIBUS UNUM",
+              "LIBERTY", "UNITED STATES OF AMERICA", "ONE CENT",
+              false, "Copper");
+    }
+}
