be54417a87d3eb526e2775083f3b13b08f149390
malloryr810 <malloryr810@gmail.com>
2025-04-07 00:01:08 -0400

    Delete coins-abc-main-final directory

--- coins-abc-main-final/src/main/Dime.java
+++
@@ -1,8 +0,0 @@
-public class Dime extends Coin {
-    public Dime(int year) {
-        super(0.10, "Dime", "IN GOD WE TRUST", year,
-              "F_Roosevelt", "Torch_Branches", "E PLURIBUS UNUM",
-              "LIBERTY", "UNITED STATES OF AMERICA", "ONE DIME",
-              true, "Cupro-Nickel");
-    }
-}
